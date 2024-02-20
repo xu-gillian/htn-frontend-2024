b0bddf2f84f29e21dddef290a9ecb1c9e2b8e2d8
gillian <gillianxu09@gmail.com>
2024-02-21 00:26:57 +0100

    change baseURL

--- client/src/api/axios.tsx
+++ client/src/api/axios.tsx
@@ -1,17 +1,6 @@
 import axios from "axios";
 
-
-// const configs = {
-//     development: {
-//         SERVER_URI: 'localhost:5000',
-//     },
-//     production: {
-//         SERVER_URI: 'HEROKU_URI',
-//     },
-// };
-
-// module.exports.config = configs[process.env.NODE_ENV]
 export default axios.create({
-    // baseURL: 'http://localhost:5001'
-    baseURL: 'https://htn-frontend-2024-c6b6c898bb5d.herokuapp.com/'
+    baseURL: 'http://localhost:5001'
+    // baseURL: 'https://htn-frontend-2024-c6b6c898bb5d.herokuapp.com/'
 });
